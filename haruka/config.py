0dc6b634d926ff52cb5f9c2fe526f91a23c8d145
Adarsh Kumar <65236741+ak47adarsh01@users.noreply.github.com>
2021-03-19 19:44:13 +0530

    Add files via upload

---
+++ haruka/config.py
@@ -0,0 +1,40 @@
+import sys
+class Config(object):
+    LOGGER = True
+
+    # REQUIRED
+    API_KEY = "1700098307:AAHAHkw-h4I6oZWTWVB0OhbeqN_sJJptjck"
+    OWNER_ID = "855675386"  # If you dont know, run the bot and do /id in your private chat with it
+    OWNER_USERNAME = "@ChevroletImpala67"
+
+    # RECOMMENDED
+    SQLALCHEMY_DATABASE_URI = 'sqldbtype://username:pw@hostname:port/db_name'  # needed for any database modules
+    MESSAGE_DUMP = None  # needed to make gban logs and etc.
+    LOAD = []
+    NO_LOAD = []
+    WEBHOOK = False
+    URL = None
+
+    # OPTIONAL
+    SUDO_USERS = []  # List of id's (not usernames) for users which have sudo access to the bot.
+    SUPPORT_USERS = []  # List of id's (not usernames) for users which are allowed to gban, but can also be banned.
+    WHITELIST_USERS = []  # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
+    MAPS_API = ''
+    CERT_PATH = None
+    PORT = 8443
+    DEL_CMDS = True # Whether or not you should delete "blue text must click" commands
+    WORKERS = 4  # Number of subthreads to use. This is the recommended amount - see for yourself what works best!
+    BAN_STICKER = 'CAADAgADOwADPPEcAXkko5EB3YGYAg'  # banhammer marie sticker
+    STRICT_ANTISPAM = True 
+    ALLOW_EXCL = True  # Allow ! commands as well as /
+    API_OPENWEATHER = None # OpenWeather API
+    
+
+
+
+class Production(Config):
+    LOGGER = False
+
+
+class Development(Config):
+    LOGGER = True
